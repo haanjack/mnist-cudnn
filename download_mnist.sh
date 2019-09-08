03bda4f1d115bdb62ec008966eca2564d6be6a31
Jae Geun Han <haanjack@gmail.com>
2019-09-09 05:08:00 +0900

    download file to the dataset directory directly

--- download_mnist.sh
+++ download_mnist.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 url_base=http://yann.lecun.com/exdb/mnist
+dataset_path=dataset
 
-mkdir -p dataset
+mkdir -p ${dataset_path}
 
-wget ${url_base}/train-images-idx3-ubyte.gz
-wget ${url_base}/train-labels-idx1-ubyte.gz
-wget ${url_base}/t10k-images-idx3-ubyte.gz
-wget ${url_base}/t10k-labels-idx1-ubyte.gz
+wget -P ${dataset_path} ${url_base}/train-images-idx3-ubyte.gz
+wget -P ${dataset_path} ${url_base}/train-labels-idx1-ubyte.gz
+wget -P ${dataset_path} ${url_base}/t10k-images-idx3-ubyte.gz
+wget -P ${dataset_path} ${url_base}/t10k-labels-idx1-ubyte.gz
 
-mv *.gz dataset
 gunzip dataset/*.gz
